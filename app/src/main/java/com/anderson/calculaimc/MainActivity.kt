e085f7ef1615a68de5deac5f5b3392766bdb7e55
Anderson Silva <andersonsilvaacaval@gmail.com>
2020-06-24 00:14:58 -0300

    layout splahActivity e ResultActivity

--- app/src/main/java/com/anderson/calculaimc/MainActivity.kt
+++ app/src/main/java/com/anderson/calculaimc/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.anderson.calculaimc
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Handler().postDelayed({
+            val mIntent = Intent(this, ResultActivity::class.java)
+            startActivity(mIntent)
+        }, 3000)
     }
 }
--- app/src/main/java/com/anderson/calculaimc/SplashActivity.kt
+++ app/src/main/java/com/anderson/calculaimc/SplashActivity.kt
@@ -1,11 +1,19 @@
 package com.anderson.calculaimc
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            val mIntent = Intent(this, MainActivity::class.java)
+            startActivity(mIntent)
+            finish()
+        }, 3000)
     }
 }
