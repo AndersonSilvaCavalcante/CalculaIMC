b8628ce7b51d257b5365a0b95e50686c47028f13
Anderson Silva <andersonsilvaacaval@gmail.com>
2020-06-25 00:12:00 -0300

    activity finalizada e função de calcular trabalhando

--- app/src/main/java/com/anderson/calculaimc/MainActivity.kt
+++ app/src/main/java/com/anderson/calculaimc/MainActivity.kt
@@ -4,15 +4,62 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.widget.SeekBar
+import android.widget.TextView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Handler().postDelayed({
-            val mIntent = Intent(this, ResultActivity::class.java)
-            startActivity(mIntent)
-        }, 3000)
+        var altura_result = 0.0
+        var peso_result = 0.0
+
+        val pesoSeekBar: SeekBar
+        pesoSeekBar = findViewById(R.id.pesoValue)
+        pesoSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                peso_result = progress.toDouble()/10
+                pesoResult.text = peso_result.toString() + "KG"
+
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+
+        })
+
+        val alturaSeekBar: SeekBar
+        alturaSeekBar = findViewById(R.id.alturaValue)
+        alturaSeekBar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                altura_result = progress.toDouble()/100
+                alturaResult.text = altura_result.toString() + "m"
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+        })
+        btnCalcular.setOnClickListener {
+            if (peso_result >0 && altura_result >0) {
+                val mIntent = Intent(this, ResultActivity::class.java)
+                mIntent.putExtra("KEY_PESO", peso_result.toString())
+                mIntent.putExtra("KEY_ALTURA", altura_result.toString())
+                startActivity(mIntent)
+            }else{
+                Toast.makeText(applicationContext, "Escolha valores válidos em altura e peso!", Toast.LENGTH_LONG).show()
+            }
+        }
+
     }
 }
--- app/src/main/java/com/anderson/calculaimc/ResultActivity.kt
+++ app/src/main/java/com/anderson/calculaimc/ResultActivity.kt
@@ -1,11 +1,36 @@
 package com.anderson.calculaimc
 
 import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_result.*
 
 class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+
+        val peso = intent.getStringExtra("KEY_PESO").toDouble()
+        val altura = intent.getStringExtra("KEY_ALTURA").toDouble()
+
+        val result_imc = peso / (altura * altura)
+        resultIMC.text = result_imc.toString()
+
+        if (result_imc < 18.5){
+            resultClassificacao.text = "Abaixo do Peso"
+        }else if (result_imc >= 18 && result_imc <= 24){
+            resultClassificacao.text = "Peso Ideal"
+        }else if (result_imc >= 25 && result_imc <= 29){
+            resultClassificacao.text = "Levemente Acima do Peso"
+        }else if(result_imc >= 30 && result_imc <= 34){
+            resultClassificacao.text= "Obesidade grau 1"
+        }else if (result_imc >= 35 && result_imc <= 39){
+            resultClassificacao.text = "Obesidade Grau 2"
+        }else{
+            resultClassificacao.text = "Obesidade 3"
+        }
+
     }
+
 }
